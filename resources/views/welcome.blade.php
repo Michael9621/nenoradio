f6d9873fe806c2906355a9face5ee4869fe9c9c8
Michael9621 <oandami@gmail.com>
2019-11-18 23:09:12 +0300

    ui side nav fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -280,14 +280,8 @@
 						<!-- /social widget -->
 						
 						<!-- subscribe widget -->
-						<div class="widget subscribe-widget">
-							<div class="widget-title">
-								<h2 class="title">Subscribe to Newslatter</h2>
-							</div>
-							<form>
-								<input class="input" type="email" placeholder="Enter Your Email">
-								<button class="input-btn">Subscribe</button>
-							</form>
+						<div class="widget">
+							<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FMwangazawanenofm%2F&tabs=timeline&width=340&height=700&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 						</div>
 						<!-- /subscribe widget -->
 						
