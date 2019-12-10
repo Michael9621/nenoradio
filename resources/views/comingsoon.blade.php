0e4136acefed78c9416767c1783220a0f65798c0
Michael9621 <oandami@gmail.com>
2019-12-10 23:53:24 +0300

    fix on comming soon and middleware routes

--- resources/views/comingsoon.blade.php
+++ resources/views/comingsoon.blade.php
@@ -1,91 +1,60 @@
-
 <!DOCTYPE html>
-
 <html>
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+}
 
-    <head>
+.bgimg {
+  background-image: url('https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&w=1000&q=80');
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: white;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 25px;
+}
 
-        <title>Mwangaza Wa Neno FM</title>
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 16px;
+}
 
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+}
 
+.middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+hr {
+  margin: auto;
+  width: 40%;
+}
+</style>
+<body>
 
+<div class="bgimg">
+  <div class="topleft">
+    <p>Mwangaza Radio</p>
+  </div>
+  <div class="middle">
+    <h1>COMING SOON</h1>
+    <hr>
+    <p>tumaini la jamii</p>
+  </div>
+  
+</div>
 
-
-        <style>
-
-            html, body {
-
-                height: 100%;
-
-            }
-
-
-
-            body {
-
-                margin: 0;
-
-                padding: 0;
-
-                width: 100%;
-
-                display: table;
-
-                font-weight: 100;
-
-                font-family: 'Lato';
-
-            }
-
-
-
-            .container {
-
-                text-align: center;
-
-                display: table-cell;
-
-                vertical-align: middle;
-
-            }
-
-
-
-            .content {
-
-                text-align: center;
-
-                display: inline-block;
-
-            }
-
-
-
-            .title {
-
-                font-size: 96px;
-                font-weight:bold;
-
-            }
-
-        </style>
-
-    </head>
-
-    <body>
-
-        <div class="container">
-
-            <div class="content">
-
-                <div class="title">Coming soon</div>
-
-            </div>
-
-        </div>
-
-    </body>
-
+</body>
 </html>
